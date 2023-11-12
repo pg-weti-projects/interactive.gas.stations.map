e338faa34b85f73b1c68ddfcb4ed9bd446fcfa3a
MeHow256 <m.m.dudziak.2@gmail.com>
2023-11-12 18:52:16 +0100

    Added logic for marking route on the map

--- WebMap/static/js/map.js
+++ WebMap/static/js/map.js
@@ -1,121 +1,205 @@
 $(document).ready(function () {
-    var ol = OpenLayers;
-
     // App settings
-    var wgs84 = new ol.Projection("EPSG:4326");
-    var options = {
-        projection: wgs84,
-        displayProjection: wgs84,
-        units: "degrees",
-    };
+    const defaultProjection = 'EPSG:4326';
 
-    var zoomLevel = 7;
+    var apiKey = 'AAPKaf77b11595124e6295c9f2679a38fb9dJbeoPRXhOddgVhIXAURQSmut9oqQkOmIzIDqSr7EK-_Vyjo3Wm_mYzt-dUi6WT49';
+
+    const zoomLevel = 7;
+    const centerPlCoords = [19, 52];
+
     var map;
-    var center_pl_lon = 19;
-    var center_pl_lat = 52;
-    var user_marker_lon = null;
-    var user_marker_lat = null;
-
-    var markersLayer;
     var drawControl = null;
+    var userMarkerCoords = null;
+    var markersLayer;
+
+    var routeLayer;
+    var roadOnMap = false;
 
     // Initialize all basic components on the website
     function init() {
-        map = new ol.Map('map', options);
-
-        var osm = new ol.Layer.OSM("OSM Map");
-        map.addLayer(osm);
-
-        // add switch menu to change layers
-        var switcher = new ol.Control.LayerSwitcher();
-        map.addControl(switcher);
-
-        // adding mouse position in left down corner ( in degrees )
-        var mouse_position = new ol.Control.MousePosition();
-        map.addControl(mouse_position);
-
-        // set poland as center of map
-        var lonLat = createLonLatObject(center_pl_lon, center_pl_lat);
-        map.setCenter(lonLat, zoomLevel);
-
-        markersLayer = new ol.Layer.Vector("Markers", {
-            styleMap: new ol.StyleMap({
-                'default': {
-                    externalGraphic: 'static/img/user_location_point.png',
-                    graphicWidth: 30,
-                    graphicHeight: 30,
-                    graphicXOffset: -15,
-                    graphicYOffset: -30,
-                    graphicZIndex: 1,
-                }
-            })
+        map = new ol.Map({
+            target: 'map',
+            projection: defaultProjection,
+            displayProjection: defaultProjection,
+            units: 'degrees',
         });
-        map.addLayer(markersLayer);
+
+        // osm layer
+        var osmLayer = new ol.layer.Tile({
+            source: new ol.source.OSM(),
+            title: 'OSM Map',
+        });
+        map.addLayer(osmLayer);
+
+        // Add layers to the map
+        addRouteLayer();
+        addUserMarkerLayer();
+
+        // ADDITIONAL OLMS MAP WITH ROUTES
+//        const basemapId = "arcgis/navigation";
+//        const basemapURL = `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapId}?token=${apiKey}`;
+//        olms.apply(map, basemapURL)
+//
+//        .then(function (map) {
+//            addRouteLayer();
+//
+//            addUserMarkerLayer();
+//        });
+
+        // set Zoom and Center Of Poland as the first view when website is loaded
+        var lonLat = ol.proj.fromLonLat(centerPlCoords);
+        map.getView().setCenter(lonLat);
+        map.getView().setZoom(zoomLevel);
+
+        // adding mouse position control
+        var mousePositionControl = new ol.control.MousePosition({
+            coordinateFormat: ol.coordinate.createStringXY(4),
+            projection: defaultProjection,
+            target: document.getElementById('mouse-position'),
+            undefinedHTML: '&nbsp;'
+        });
+        map.addControl(mousePositionControl);
 
         $(".add-marker").click(function () {
-            showAlert("Place marker on the map", "info");
-            enableMarkerAddMode();
+            showAlert('Place marker on the map', 'info');
+            addUserMarker();
         });
-    }
 
-    // Enable Marker mode to add marker on the map
-    function enableMarkerAddMode() {
-        if (drawControl) {
-            map.removeControl(drawControl);
-            drawControl.deactivate();
-            drawControl.destroy();
-            drawControl = null;
-        }
+        $('.find-nearest-station').click(function () {
+            if (userMarkerCoords !== null) {
 
-        drawControl = new ol.Control.DrawFeature(markersLayer, ol.Handler.Point);
-        map.addControl(drawControl);
-        drawControl.activate();
+                // if road is on the map, we need to remove it before mark another route
+                removeRouteIfExist();
 
-        drawControl.events.on({
-            featureadded: function (event) {
-                var feature = event.feature;
-                var user_marked_lonlat = feature.geometry.clone().transform(map.getProjectionObject(), wgs84);
+                const stationCoords = [18.7256813, 53.4409896] // TODO TEMP COORDS UNTIL WE DONT HAVE FINDING NEAREST STATION FUNC THAT RETURN COORDS
 
-                // set marker lon and lat to global variables
-                user_marker_lon = user_marked_lonlat.x;
-                user_marker_lat = user_marked_lonlat.y;
+                updateRoute(userMarkerCoords, stationCoords)
 
-                map.removeControl(drawControl);
-                drawControl.deactivate();
-                drawControl.destroy();
-                drawControl = null;
+                showAlert('Found nearest station!', 'success');
 
-                markersLayer.removeAllFeatures();
-                markersLayer.addFeatures(feature);
-                $(".add-marker").text("Change Marker Location");
-                $('.find-nearest-station').prop('disabled', false);
-                showAlert(`Successfully placed marker (${user_marker_lon}, ${user_marker_lat})`, "success");
+            } else {
+                showAlert('Please place a marker first.', 'warning');
             }
         });
     }
 
-    // Creating lon lat object with correct projection
-    function createLonLatObject(lon, lat) {
-        return new ol.LonLat(lon, lat).transform(wgs84, map.getProjectionObject());
+
+    /*Add layer to draw route on the map between two points*/
+    function addRouteLayer() {
+        routeLayer = new ol.layer.Vector({
+            style: new ol.style.Style({
+                stroke: new ol.style.Stroke({ color: "hsl(205, 100%, 50%)", width: 4, opacity: 0.6 })
+            })
+        });
+        map.addLayer(routeLayer);
     }
 
-    // Listens if remove-marker button has been clicked and remove user marker from map.
+
+    /*Add layer to put user marker on the map*/
+    function addUserMarkerLayer() {
+        markersLayer = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+            style: new ol.style.Style({
+                image: new ol.style.Icon({
+                    src: 'static/img/user_location_point.png',
+                    scale: 0.08,
+                    anchor: [0.5, 1],
+                }),
+            }),
+            title: 'Markers Vector Layer'
+        });
+        map.addLayer(markersLayer);
+    }
+
+
+    /*Add route to routeLayer between two points (default user marker and station marker)*/
+    function updateRoute(userMarkerCoords, stationCoords) {
+
+        const geojson = new ol.format.GeoJSON({
+            defaultDataProjection: defaultProjection,
+            featureProjection: "EPSG:3857"
+        });
+
+        const authentication = arcgisRest.ApiKeyManager.fromKey(apiKey);
+        arcgisRest
+          .solveRoute({
+            stops: [userMarkerCoords, stationCoords],
+            authentication
+        })
+
+        .then((response) => {
+            routeLayer.setSource(
+                new ol.source.Vector({
+                    features: geojson.readFeatures(response.routes.geoJson)
+                })
+            );
+        })
+        roadOnMap = true;
+    }
+
+
+    /*Remove route from the map if exist*/
+    function removeRouteIfExist() {
+        if (roadOnMap) {
+            routeLayer.getSource().clear();
+            roadOnMap = false;
+        }
+    }
+
+
+    /*Function activate drawing mode to add marker in place selected by user.*/
+    function addUserMarker() {
+        if (drawControl) {
+            map.removeControl(drawControl);
+            drawControl.setActive(false);
+            map.removeInteraction(drawControl);
+            drawControl = null;
+        }
+
+        drawControl = new ol.interaction.Draw({
+            source: markersLayer.getSource(),
+            type: 'Point',
+        });
+
+        map.addInteraction(drawControl);
+        drawControl.setActive(true);
+
+        drawControl.on('drawend', function (event) {
+            var user_marked_lonlat = event.feature.getGeometry().clone().transform('EPSG:3857', defaultProjection);
+
+            userMarkerCoords = user_marked_lonlat.getCoordinates();
+
+            map.removeInteraction(drawControl);
+            drawControl.setActive(false);
+
+            markersLayer.getSource().clear();
+
+            $(".add-marker").text('Change Marker Location');
+            $('.find-nearest-station').prop('disabled', false);
+
+            removeRouteIfExist();
+            showAlert(`Successfully placed marker (${userMarkerCoords[0]}, ${userMarkerCoords[1]})`, 'success');
+        });
+    }
+
+
+    /*Listens if remove-marker button has been clicked and removes the user marker from the map.*/
     $(".remove-marker").click(function () {
-    if (user_marker_lon !== null && user_marker_lat !== null) {
-        markersLayer.removeAllFeatures();
+        if (userMarkerCoords !== null) {
+            removeRouteIfExist();
+            markersLayer.getSource().clear();
 
-        user_marker_lon = null;
-        user_marker_lat = null;
+            userMarkerCoords = null;
 
-        $('.find-nearest-station').prop('disabled', true);
+            $('.find-nearest-station').prop('disabled', true);
 
-        $(".add-marker").text("Add marker");
+            $(".add-marker").text('Add marker');
 
-        showAlert("Marker removed", "info");
-    } else {
-        showAlert("No marker to remove", "warning");
-    }
-});
+            showAlert('Marker removed', 'info');
+        } else {
+            showAlert('No marker to remove', 'warning');
+        }
+    });
 
     init();
 });
