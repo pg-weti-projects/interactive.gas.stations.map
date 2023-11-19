e2144fb71813117cd8f4b58f60ff327d383ea029
MeHow256 <m.m.dudziak.2@gmail.com>
2023-11-19 19:08:58 +0100

    Fixed comments in app.py

--- WebMap/app.py
+++ WebMap/app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, jsonify, redirect, url_for
 
 from osm_manager import OsmManager
@@ -7,10 +8,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def interactive_map():
+def interactive_map() -> flask.Response | str:
     """
-    main website page.
-    check that database exists before launching the website
+    Main website page with map. Check that database exists before launching the website. If not redirect to
+    create_database first.
     """
     mongo_manager = MongoManager()
     if mongo_manager.is_database_exist > 0:
@@ -22,9 +23,9 @@ def interactive_map():
 
 
 @app.route("/api/gas_station_data")
-def data_map():
+def data_map() -> flask.Response:
     """
-        Route api with gas stations data.
+    Route api with gas stations data.
     """
     mongo_manager = MongoManager()
     data = mongo_manager.get_records_from_db()
@@ -32,9 +33,9 @@ def data_map():
 
 
 @app.route("/database")
-def create_database():
+def create_database() -> flask.Response:
     """
-            Route witch creating / updating database.
+    Route witch creating / updating database.
     """
     osm_manager = OsmManager()
     mongo_manager = MongoManager()
